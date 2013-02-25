7d6d5ebd2b6a450e0ff7e54568dd58a752033a19
jspc <james@zero-internet.org.uk>
2013-02-25 14:11:40 +0000

    Simple AR object

--- lib/note.rb
+++ lib/note.rb
@@ -2,5 +2,20 @@
 #
 #
 
+require 'active_record'
+require 'redis'
+require 'redis/objects'
+require 'redis/list'
+
+Redis.current = Redis.new(:host => '127.0.0.1', :port => 6379)
+
 class Note < ActiveRecord::Base
   include Redis::Objects
+  
+  value :task
+  value :location
+  value :time
+  value :length
+  list  :attendees
+
+end
