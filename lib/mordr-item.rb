84bdf1f3ca164c757fd7a5e09b1732051c5174b0
jspc <james@zero-internet.org.uk>
2013-03-29 18:17:22 +0000

    Basic model

--- lib/mordr-item.rb
+++ lib/mordr-item.rb
@@ -3,30 +3,65 @@
 # Todo List Item controller
 
 module MordrItem
-  def find_item list_o, text
-    item_o = list_o.items
-    return item_o unless item_o.nil?
+
+  def _list_object handle, slug
+    MordrList::find_list handle, slug
+  end
+
+  def all_items handle, slug
+    if list_o = _list_object( handle, slug )
+      return list_o.items
+    end
+    return []
+  end
+
+  def find_item handle, slug, text
+    all_items( handle, slug ).each do |item|
+      return item if item.text == text
+    end
     return false
   end
 
-  def new_item list_o, text, desc
-    return nil if find_item( list_o, text )
-    item_o = list_o.create( :items => [ Item.new(
-                                                 :text => text,
-                                                 :desc => desc
-                                                 )
-                            ])
-    item_o.save
+  def find_item_by_id handle, slug, id
+    all_items( handle, slug ).each do |item|
+      return item if item._id.to_s == id
+    end
+    return false
   end
 
-  def del_item list_o, text
-    item_o = find_item list_o, text
-    item_o.destroy if item_o
+  def add_item handle, slug, text, desc
+    return nil if find_item( handle, slug, text )
+
+    list_o = _list_object( handle, slug )
+    list_o.items << Item.new(
+                             :text => text,
+                             :desc => desc
+                             )
+    list_o.save
   end
 
-  def set_done list_o, title
-    item_o = find_item list_o, text
-    item_o.done = true if item_o
-    item_o.save
+  def delete_item handle, slug, id
+    list_o = _list_object handle, slug
+    if list_o
+      list_o.items.delete_if{ |item| item._id.to_s == id }
+      list_o.save
+    end
   end
+
+  def set_done handle, slug, id
+    item_o = find_item_by_id( handle, slug, id )
+    if item_o
+      item_o.done = true
+      item_o.save
+    end
+  end
+
+  def set_not_done handle, slug, id
+    item_o = find_item_by_id( handle, slug, id )
+    if item_o
+      item_o.done = false
+      item_o.save
+    end
+  end
+
 end
