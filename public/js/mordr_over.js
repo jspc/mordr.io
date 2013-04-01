c49d2ef23ed2fc84aaad478ff3699b4a4420f1b5
jspc <james@zero-internet.org.uk>
2013-04-01 13:02:45 +0100

    Add colorbox

---
+++ public/js/mordr_over.js
@@ -0,0 +1,10 @@
+(jQuery(function($) {
+	//	alert( "Started!" );
+	
+	$(".add_item").colorbox({
+		iframe:true,
+		innerWidth: 300,
+		innerHeight: 400,    
+	    });
+	//	alert( "Got this far!" );
+    })(jQuery));
