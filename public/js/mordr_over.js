c4e31bdfcbe2316d918c5e8fc23b88fb66b14581
jspc <james@zero-internet.org.uk>
2013-04-01 13:34:10 +0100

    General beautifications

--- public/js/mordr_over.js
+++ public/js/mordr_over.js
@@ -4,7 +4,8 @@
 	$(".add_item").colorbox({
 		iframe:true,
 		innerWidth: 300,
-		innerHeight: 400,    
+		innerHeight: 400,
+		onClosed:function(){ location.reload(true); },
 	    });
 	//	alert( "Got this far!" );
     })(jQuery));
