2bd0630d0de9618281231f4d38e6427c04e9e7a2
jspc <james@zero-internet.org.uk>
2013-04-07 17:13:49 +0100

    Write a small search engine

--- app/routes/search_routes.rb
+++ app/routes/search_routes.rb
@@ -6,7 +6,7 @@ get '/search/users.json' do
     user_list.push( {
                       'value'  => user.handle,
                       'user'   => user.handle,
-                      'tokens' => [user.handle]
+                      'tokens' => [user.handle],
                     } )
   end
   content_type :json
@@ -22,10 +22,35 @@ get '/search/lists.json' do
                         'value'  => list.title,
                         'user'   => user.handle,
                         'slug'   => list.slug,
-                        'tokens' => [user.handle, list.title, list.slug, list.describe]
+                        'tokens' => [user.handle, list.title, list.slug, list.describe],
                       } ) if list.public?
     end
   end
   content_type :json
   list_list.to_json
+end
+
+get '/search/' do
+  erb :'search/search_get'
+end
+
+post '/search/' do
+  @search_term = params[:search]
+  @user_list   = []
+  @list_list   = []
+
+  all_users.each do |user|
+    @user_list.push( user.handle ) if user.handle =~ /#{@search_term}/
+    all_lists( user.handle ).each do |list|
+      if list.title =~ /#{@search_term}/ or list.slug =~ /#{@search_term}/ or list.describe =~ /#{@search_term}/ or user.handle =~ /#{@search_term}/
+        @list_list.push ( {
+                            :handle => user.handle,
+                            :slug   => list.slug,
+                            :title  => list.title,
+                          } ) if list.public?
+      end
+    end
+  end
+  erb :'search/search'
+
 end
