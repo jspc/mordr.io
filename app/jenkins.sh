7e09a57ba79976f44ca728507832eadbaaf312d9
jspc <james@zero-internet.org.uk>
2013-10-12 19:13:12 +0100

    Split out for easier testing

---
+++ app/jenkins.sh
@@ -0,0 +1,19 @@
+# Build env
+export VERS=$(cat .ruby-version)
+
+[ $(rbenv local $VERS) ] || rbenv install $VERS
+rbenv local $VERS
+echo -e "\n\n$(ruby -v)\n\n"
+
+bundle
+rake -T
+rake clean
+
+# Static analysis
+excellent **/*
+if [ "$(excellent **/* | grep warnings | awk '{print $2}')" -gt "30" ]; then
+  exit 1;
+fi
+
+# Tests
+rspec --order rand --format d
